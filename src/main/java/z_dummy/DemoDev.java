d3f6db16b824646c1e215c7af48ba0e6f1827c03
patil-kaustubh <kaustubh.patil7@gmail.com>
2025-10-29 11:06:34 +0530

    commitMSG

---
+++ src/main/java/z_dummy/DemoDev.java
@@ -0,0 +1,10 @@
+package z_dummy;
+
+public class DemoDev {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+	}
+
+}
---
+++ src/test/java/z_dummy/DemoTest.java
@@ -0,0 +1,5 @@
+package z_dummy;
+
+public class DemoTest {
+
+}
